1597ebfdc9f7af0e2cfb8a3393b7ed2dc8b71029
Rotem Bar <rotem68907@gmail.com>
2021-05-04 19:18:17 +0300

    second commit

--- App.jsx
+++ App.jsx
@@ -3,14 +3,14 @@ const { Route, Switch } = ReactRouterDOM
 
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
-import { AppKeep } from './apps/Keeps/App.jsx'
-import { AppBook } from './apps/Books/pages/BookApp.jsx'
-import { AppEmail } from './apps/Emails/App.jsx'
+import { KeepApp } from './apps/Keeps/KeepApp.jsx'
+import { BookApp } from './apps/Books/BookApp.jsx'
+// import { AppEmail } from './apps/Emails/cmps/EmailsApp.jsx'
 
 function Home() {
     return <section>
         <h1>Home appSus</h1>
-       
+
     </section>
 }
 
@@ -18,14 +18,13 @@ export function App() {
     return (
         <Router>
             <header>
-                <AppHeader/>
+                <AppHeader />
             </header>
             <main>
                 <Switch>
-                
-                    {/* <Route component={AppKeep} path="/missKeep" /> */}
-                    {/* <Route component={MisterEmail} path="/misterEmail" /> */}
-                    <Route component={AppBook} path="/missBook" />
+                    <Route component={KeepApp} path="/missKeep" />
+                    {/* <Route component={AppEmail} path="/misterEmail" /> */}
+                    {/* <Route component={AppBook} path="/missBook" /> */}
                     {/* <Route component={AboutUs} path="/about" /> */}
                     <Route component={Home} path="/" />
                 </Switch>
