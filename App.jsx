6ffe412c4685f4125709aa25cf3fef76af53b42b
noakap <noakap92@gmail.com>
2021-05-04 19:34:16 +0300

    after first pull

--- App.jsx
+++ App.jsx
@@ -3,14 +3,14 @@ const { Route, Switch } = ReactRouterDOM
 
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
-// import { AppKeep } from './apps/Keeps/App.jsx'
-// import { AppBook } from './apps/Books/pages/BookApp.jsx'
 import { AppEmail } from './apps/Emails/pages/MailsApp.jsx'
+import { KeepApp } from './apps/Keeps/KeepApp.jsx'
+import { BookApp } from './apps/Books/BookApp.jsx'
 
 function Home() {
     return <section>
         <h1>Home appSus</h1>
-       
+
     </section>
 }
 
@@ -18,13 +18,12 @@ export function App() {
     return (
         <Router>
             <header>
-                <AppHeader/>
+                <AppHeader />
             </header>
             <main>
                 <Switch>
-                
-                    {/* <Route component={AppKeep} path="/missKeep" /> */}
                     <Route component={AppEmail} path="/misterEmail" />
+                    <Route component={KeepApp} path="/missKeep" />
                     {/* <Route component={AppBook} path="/missBook" /> */}
                     {/* <Route component={AboutUs} path="/about" /> */}
                     <Route component={Home} path="/" />
