581e1b857d2f98d9383bbec04030da3c45ebe461
Rotem Bar <rotem68907@gmail.com>
2021-05-08 18:52:40 +0300

    css

--- cmps/Home.jsx
+++ cmps/Home.jsx
@@ -8,12 +8,12 @@ export class Home extends React.Component {
     return (
       <section className="home-page">
           <Link className="email-app-link" to='/misterEmail'>
-            Click here to join our 100,000+ users and start your First {'&'} Truly orgenaized E-mail.
+            Click here to join our 10,000+ users and start your First organised E-mail.
             <img className='email-bg-image' src="./assets/img/email-homepage.png" alt="" />
           </Link>
           <hr/>
           <Link className="keep-app-link" to='/missKeep'>
-            Try now our notes feature, It's waiting for you only One click away!
+            Try now our notes feature, It's waiting for you only One click away! 
           <img className='keep-bg-image' src="./assets/img/notes-homepage.jpg" alt="" />
           </Link>
       </section>
